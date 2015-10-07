f47e9ee7b302f9f0e90409ea5defb7df10ced42d
Timothy Kim <timothy.kim@washpost.com>
2015-10-06 21:28:59 -0400

    lecture 3

---
+++ DateMe/src/main/java/dateme/RGB.java
@@ -0,0 +1,13 @@
+package dateme;
+
+public class RGB {
+    private int red;
+    private int green;
+    private int blue;
+
+    public RGB(int r, int g, int b) {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+}
---
+++ DateMe/src/main/java/dateme/User.java
@@ -0,0 +1,16 @@
+package dateme;
+
+public class User {
+
+    public final String email;
+    public final RGB color;
+    public final int number;
+
+    public User(String e, RGB c, int n) {
+        this.email = e;
+        this.color = c;
+        this.number = n;
+    }
+
+
+}
